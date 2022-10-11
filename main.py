c9bfc1b98aeac81aa327df7fbf5082bf13f658bd
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-11 11:56:22 -0700

    update manually to github

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
+SUBSET = True
+
+for i in range(len(num2)):
+    if num2[i] not in num1:
+        SUBSET = False
+        break
+
+if SUBSET == True:
+    statement = "is"
+else:
+    statement = 'is not'
+
+message = f"num2 {statement} a subset of num1"
+print(message)
+print(str(SUBSET))
+
 # ******************************
 # Make your Code
 # ******************************
